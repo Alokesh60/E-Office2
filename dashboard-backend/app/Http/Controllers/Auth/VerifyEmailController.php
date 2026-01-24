186d7855d9403257d3aab9b7d6ea425d28c6acfe
Alokesh60 <alokeshlk60@gmail.com>
2026-01-24 14:03:02 +0530

    Fix dashboard, auth, and api issues

--- dashboard-backend/app/Http/Controllers/Auth/VerifyEmailController.php
+++ dashboard-backend/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,26 +6,31 @@ use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(Request $request, $id, $hash)
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(
-                config('app.frontend_url').'/dashboard?verified=1'
-            );
+        $user = User::findOrFail($id);
+
+        if (! hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+            abort(403, 'Invalid verification link.');
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->route('login')
+                ->with('status', 'Email already verified. Please login.');
         }
 
-        return redirect()->intended(
-            config('app.frontend_url').'/dashboard?verified=1'
-        );
+        $user->markEmailAsVerified();
+        event(new verified($user));
+
+        return redirect()->route('login')
+            ->with('status', 'Email verified successfully. Please login');
     }
 }
