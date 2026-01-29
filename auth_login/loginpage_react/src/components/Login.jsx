7a2b337fbea2eca6f9138176e41abc1f1a8fdc30
Bugmita-Bora <borabugmita009@gmail.com>
2026-01-30 00:58:43 +0530

    added the remaining input box in register

--- auth_login/loginpage_react/src/components/Login.jsx
+++ auth_login/loginpage_react/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
@@ -8,39 +8,22 @@ function Login() {
     <div className={`container ${isActive ? "active" : ""}`}>
       {/* LOGIN FORM */}
       <div className="form-box login">
-        <form action="">
+        <form action="#">
           <h1>Login</h1>
           <div className="input-box">
             <input type="text" placeholder="Username" required />
             <i className="bx bx-user"></i>
           </div>
-
-          <div className="input-box">
-            {/* Added defaultValue to fix React warning */}
-            <select required defaultValue="">
-              <option value="" disabled>
-                Select your role
-              </option>
-              <option value="student">Student</option>
-              <option value="teacher">Finance Officer</option>
-              <option value="dean">Dean</option>
-              <option value="academic">Academic Administrator</option>
-            </select>
-          </div>
-
           <div className="input-box">
             <input type="password" placeholder="Password" required />
             <i className="bx bxs-lock-alt"></i>
           </div>
-
           <div className="forget-link">
             <a href="#">Forgot password?</a>
           </div>
-
           <button type="submit" className="btn">
             Login
           </button>
-
           <p className="google-login">Log in with Google</p>
           <div className="social-icons">
             <a href="#">
@@ -50,14 +33,31 @@ function Login() {
         </form>
       </div>
 
-      {/* REGISTER FORM */}
+      {/* REGISTER FORM - All 'class' changed to 'className' */}
       <div className="form-box register">
-        <form action="">
-          <h1>Register</h1>
+        <form action="#">
+          <h1 className="register-head">Register</h1>
           <div className="input-box">
             <input type="text" placeholder="Username" required />
             <i className="bx bx-user"></i>
           </div>
+
+          <div className="input-box">
+            {/* Fixed: Used defaultValue instead of selected attribute */}
+            <select name="role" required defaultValue="">
+              <option value="" disabled hidden>
+                Select Role
+              </option>
+              <option value="student">Student</option>
+              <option value="finance-officer">Finance Officer</option>
+              <option value="dean">Dean</option>
+              <option value="academic-administrator">
+                Academic Administrator
+              </option>
+            </select>
+            <i className="bx bxs-user-detail"></i>
+          </div>
+
           <div className="input-box">
             <input type="email" placeholder="Email" required />
             <i className="bx bx-envelope"></i>
@@ -66,11 +66,13 @@ function Login() {
             <input type="password" placeholder="Password" required />
             <i className="bx bxs-lock-alt"></i>
           </div>
-
+          <div className="input-box">
+            <input type="password" placeholder="Confirm Password" required />
+            <i className="bx bxs-lock-alt"></i>
+          </div>
           <button type="submit" className="btn">
             Register
           </button>
-
           <p className="google-login">Register with Google</p>
           <div className="social-icons">
             <a href="#">
@@ -80,28 +82,20 @@ function Login() {
         </form>
       </div>
 
-      {/* TOGGLE PANEL - Typo Fixed Here */}
+      {/* TOGGLE PANEL */}
       <div className="toggle-box">
         <div className="toggle-panel toggle-left">
           <h1>Hello, Welcome!</h1>
-          <p style={{ color: "white" }}>Don't have an account?</p>
-          <button
-            className="btn"
-            style={{ color: "white", borderColor: "white" }}
-            onClick={() => setIsActive(true)}
-          >
+          <p>Don't have an account?</p>
+          <button className="btn hidden" onClick={() => setIsActive(true)}>
             Register
           </button>
         </div>
 
         <div className="toggle-panel toggle-right">
           <h1>Welcome Back!</h1>
-          <p style={{ color: "white" }}>Already have an account?</p>
-          <button
-            className="btn"
-            style={{ color: "white", borderColor: "white" }}
-            onClick={() => setIsActive(false)}
-          >
+          <p>Already have an account?</p>
+          <button className="btn hidden" onClick={() => setIsActive(false)}>
             Login
           </button>
         </div>
