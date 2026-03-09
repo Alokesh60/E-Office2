58ef8fed4e051a7721197b88bd4f3ee87e450ade
Bugmita-Bora <borabugmita009@gmail.com>
2026-03-09 22:08:32 +0530

    updated help and support

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DashboardLayout from './components/DashboardLayout';
-import Dashboard from './pages/Dashboard';
-import Account from './pages/Account';
-import Security from './pages/Security';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardLayout from "./components/DashboardLayout";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Security from "./pages/Security";
 import Settings from "./pages/Settings";
 import About from "./pages/About";
+import HelpAndSupport from "./pages/HelpAndSupport";
 
 export default function App() {
   return (
@@ -17,9 +18,10 @@ export default function App() {
           <Route path="account" element={<Account />}>
             <Route path="security" element={<Security />} />
             <Route path="about" element={<About />} />
+            {/* Change this path to "help" to match the Account.jsx NavLink */}
+            <Route path="help" element={<HelpAndSupport />} />
           </Route>
           <Route path="settings" element={<Settings />} />
-
         </Route>
       </Routes>
     </BrowserRouter>
